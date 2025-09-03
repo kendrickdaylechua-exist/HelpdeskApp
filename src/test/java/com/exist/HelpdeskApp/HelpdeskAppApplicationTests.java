cca4c8580df8bd8f88ee304994d53f72e5f7f808
Kendrick Dayle Chua <kendrick.chua@exist.com>
2025-09-03 16:27:35 +0800

    First commit

---
+++ src/test/java/com/exist/HelpdeskApp/HelpdeskAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exist.HelpdeskApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
